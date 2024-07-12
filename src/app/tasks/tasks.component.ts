3199c3e6cf5596ba45ea21bb0547881a139ade54
kpuyam <kelvin.puyam@tigeranalytics.com>
2024-07-12 20:58:52 +0530

    Add Tasks Component

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks: any[] = [];
+
+  constructor(private route: ActivatedRoute, private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    const projectId = +this.route.snapshot.paramMap.get('id')!;
+    this.apiService.getTasks(projectId).subscribe((data: any[]) => {
+      this.tasks = data;
+    });
+  }
+}
