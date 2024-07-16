27e51d9f9c21503f5b3a99954b94eea64ab42706
kpuyam <kelvin.puyam@tigeranalytics.com>
2024-07-16 13:38:25 +0530

    Update Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProjectsComponent } from './projects/projects.component';
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/projects', pathMatch: 'full' },
-  { path: 'projects', component: ProjectsComponent },
-  { path: 'projects/:id', component: ProjectDetailComponent },
-  { path: 'projects/:id/tasks', component: TasksComponent }
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'projects', component: ProjectsComponent, canActivate: [AuthGuard] },
+  { path: 'projects/:id', component: ProjectDetailComponent, canActivate: [AuthGuard] },
+  { path: 'projects/:id/tasks', component: TasksComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
