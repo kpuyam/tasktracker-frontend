fa6b7d7ae039ca19943174e209f8919a840bfcfc
kpuyam <kelvin.puyam@tigeranalytics.com>
2024-07-16 13:39:18 +0530

    Update Login Components

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  hide = true;
+
+  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      this.authService.login(username, password).subscribe(
+        () => {
+          this.router.navigate(['/projects']);
+        },
+        () => {
+          this.loginForm.setErrors({ 'invalidLogin': true });
+        }
+      );
+    }
+  }
+}
