4425b974f498f2dcf61223f9684538e1ff71d209
kpuyam <kelvin.puyam@tigeranalytics.com>
2024-07-18 11:26:43 +0530

    Update App Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,12 @@ import { TasksComponent } from './main/projects-section/tasks/tasks.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './auth.guard';
 import { MainComponent } from './main/main.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
 ];
 
 @NgModule({
