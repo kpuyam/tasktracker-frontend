e95285950f10085610f344da52acd48fe4495743
kpuyam <kelvin.puyam@tigeranalytics.com>
2024-07-21 20:10:11 +0530

    Updated Project Detail Component

--- src/app/main/projects-section/project-detail/project-detail.component.ts
+++ src/app/main/projects-section/project-detail/project-detail.component.ts
@@ -9,13 +9,24 @@ import { ApiService } from '../../../api.service';
 })
 export class ProjectDetailComponent implements OnInit {
   project: any = {};
+  users: any = {};
 
   constructor(private route: ActivatedRoute, private apiService: ApiService) { }
 
   ngOnInit(): void {
     const projectId = +this.route.snapshot.paramMap.get('id')!;
+
     this.apiService.getProject(projectId).subscribe((data: any) => {
       this.project = data;
+
+      this.apiService.getUsers().subscribe((users: any[]) => {
+        this.users = users.reduce((acc, user) => {
+          acc[user.id] = user.username;
+          return acc;
+        }, {});
+
+        this.project.owner = this.users[this.project.owner] || 'Unknown';
+      });
     });
   }
 }
